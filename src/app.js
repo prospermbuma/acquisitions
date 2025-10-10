38a24ddec904bfd10632a6b366ccfb529966d018
prospermbuma <prospermbuma@gmail.com>
2025-10-11 02:53:56 +0300

    Import logger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import logger from './config/logger.js';
 
 // Initialize Express app
 const app = express();
 
 // Define a route
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions API!');
+
   res.status(200).send('Hello from Acquisitions API!');
 });
 

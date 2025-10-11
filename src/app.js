426e98ff2a26b79a01d2e73ca5fecd59071abf3d
prospermbuma <prospermbuma@gmail.com>
2025-10-11 03:15:15 +0300

    Add cors and cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import logger from '#config/logger.js';
 
 // Initialize Express app
@@ -9,6 +11,12 @@ const app = express();
 // Middleware for security headers
 app.use(helmet());
 
+// Middleware for enabling CORS
+app.use(cors());
+
+// Middleware for parsing cookies
+app.use(cookieParser());
+
 // Middleware for parsing JSON requests
 app.use(express.json());
 
@@ -16,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware for logging HTTP requests
-app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
 
 // Define a route
 app.get('/', (req, res) => {
